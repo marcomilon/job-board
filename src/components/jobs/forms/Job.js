b2a4f508973e6b5d9f4b3de6fca7049b2474167f
Marco Milon <marco.milon@gmail.com>
2018-06-14 16:02:45 -0500

    Job form

--- src/components/jobs/forms/Job.js
+++ src/components/jobs/forms/Job.js
@@ -1,68 +1,121 @@
 import React from 'react'
 import Form from 'react-jsonschema-form'
+import Logo from '../../common/Logo.js'
+import './job.sass'
 
 function Job(props) {
     
     const schema = {
-        type: "object",
-        title: "Publica tu empleo",
-        properties: {
-            title: { 
-                type: "string", 
-                title: "Titulo" 
+        "type": "object",
+        "title": "Publica tu empleo",
+        "properties": {
+            "job": {
+                "type": "object",
+                "title": "Cuentanos sobre el puesto de trabajo",
+                "properties": {
+                    "category": {
+                        "type": "string",
+                        "title": "Categoria",
+                        "enum": [
+                            "Freelancer",
+                            "Part time",
+                            "Full time",
+                        ]
+                    },
+                    "title": { 
+                        "type": "string", 
+                        "title": "Titulo" 
+                    },
+                    "place": { 
+                        "type": "string", 
+                        "title": "Ubicación" 
+                    },            
+                    "description": { 
+                        "type": "string", 
+                        "title": "Descripción" 
+                    },
+                    "howtoapply": { 
+                        "type": "string", 
+                        "title": "¿Como postular al trabajo?" 
+                    },
+                }
             },
-            category: {
-                type: "string",
-                title: "Categoria",
-                enum: [
-                    "Freelancer",
-                    "Part time",
-                    "Full time",
-                ]
-            },
-            place: { 
-                type: "string", 
-                title: "Ubicación" 
-            },            
-            description: { 
-                type: "string", 
-                title: "Descripción" 
-            },
-        }
+            "company": {
+                "type": "object",
+                "title": "Cuentanos sobre tu empresa",
+                "properties": {
+                    "name": { 
+                        "type": "string", 
+                        "title": "Nombre" 
+                    },
+                    "slogan": {
+                        "type": "string", 
+                        "title": "Slogan de la empresa"
+                    },
+                    "logo": {
+                        "type": "string",
+                        "format": "data-url",
+                        "title": "Logo"
+                    },
+                    "url": { 
+                        "type": "string", 
+                        "title": "Página de tu empresa" 
+                    },
+                    "email": { 
+                        "type": "string", 
+                        "title": "Correo electrónico" 
+                    },
+                    "description": {
+                        "type": "string",
+                        "title": "Algo más de tu empresa"
+                    }
+                }
+            }
+        }    
     };
     
     const uiSchema = {
-        title: {
-            classNames: "narrow-input"
-        },
-        category: {
-            "ui:widget": "radio",
-        },
-        title: {
-            classNames: "narrow-input"
-        }, 
-        place: {
-            classNames: "narrow-input"
-        },                
-        description: {
-            "ui:widget": "textarea",
-            "ui:options": {
-                rows: 5
+        "job": {                
+            "description": {
+                "classNames": "wide-input",
+                "ui:widget": "textarea",
+                "ui:options": {
+                    "rows": 5
+                }
+            },
+            "howtoapply": {
+                "classNames": "wide-input",
+                "ui:widget": "textarea"
             }
         },
+        "company": {
+            "slogan": {
+                "ui:widget": "textarea"
+            },
+            "description": {
+                "classNames": "wide-input",
+                "ui:widget": "textarea",
+                "ui:options": {
+                    "rows": 5
+                }
+            }
+        }
     }
         
     return (             
-        <div className="row ml-5 mr-5">
+        <div className="row ml-5 mr-5 mb-5">
+            <div className="col-md-6">
+                <Logo className="mt-5" />
+            </div>
             <div className="col-md-12">
-                <p className="mt-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p className="mt-5 job-presentation">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
             </div>
             <div className="col-md-12">
-                <Form schema={schema} uiSchema={uiSchema} onSubmit={props.submitJob}>
+                <Form className="job-form" schema={schema} uiSchema={uiSchema} onSubmit={props.submitJob}>
                     <div className="text-right">
-                        <button className="btn btn-primary" type="submit">Submit</button>
+                        <button className="btn btn-primary" type="submit">Continuar</button>
                     </div>
                 </Form>
             </div>
