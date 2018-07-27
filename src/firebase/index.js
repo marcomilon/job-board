ad085706945feea34f9b94097cea8fad2aa85674
Marco Milon <marco.milon@gmail.com>
2018-07-27 10:44:05 -0500

    Remove config from source code

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase';
 
-const config = {
-    apiKey: "AIzaSyB-Zs2r1iFFKJGBCp0PDY7GNNBw513RS9w",
-    authDomain: "fsjobs-e242a.firebaseapp.com",
-    databaseURL: "https://fsjobs-e242a.firebaseio.com",
-    projectId: "fsjobs-e242a",
-    storageBucket: "fsjobs-e242a.appspot.com",
-    messagingSenderId: "589707820439"
-  };
+import config from './config.js'
 
 firebase.initializeApp(config)
 
